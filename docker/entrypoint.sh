dce3abb635ab53ab8ba9ebd2241f3b0063ff1056
Jun Kurihara <kurihara@ieee.org>
2021-07-12 13:59:44 +0900

    add docker files

---
+++ docker/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "start DoH proxy"
+
+# read custom configuration
+source /opt/doh-proxy/etc/.env
+
+echo "doh-proxy: upstream dns server address: ${UPSTREAM_ADDR}:${UPSTREAM_PORT}"
+
+/opt/doh-proxy/sbin/doh-proxy \
+  --server-address=${UPSTREAM_ADDR}:${UPSTREAM_PORT} \
+  --listen-address=0.0.0.0:3000 \
+  --validation-algorithm=${VALIDATION_ALGORITHM} \
+  --validation-key-path=/opt/doh-proxy/etc/public_key.pem
